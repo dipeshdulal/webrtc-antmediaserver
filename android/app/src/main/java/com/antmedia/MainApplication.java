ec3556b0b5eda76fa75084c26f5e9b23da3b1c88
esprajwol <esprajwol@gmail.com>
2021-03-22 12:08:36 +0545

    chore: run on android

--- android/app/src/main/java/com/antmedia/MainApplication.java
+++ android/app/src/main/java/com/antmedia/MainApplication.java
@@ -20,8 +20,11 @@ public class MainApplication extends Application implements ReactApplication {
           return BuildConfig.DEBUG;
         }
 
+
+
         @Override
         protected List<ReactPackage> getPackages() {
+          
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
