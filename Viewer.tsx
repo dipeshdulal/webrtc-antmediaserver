28971cdd220ddadd5a9fc4ebefe7d7e4428265e1
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-03-22 20:10:15 +0545

    feat: added viewer component

---
+++ Viewer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export const Viewer = () => {
+    return (
+        <View style={StyleSheet.absoluteFill}>
+            <Text>
+                Viewer Screen
+             </Text>
+        </View>
+    )
+}
